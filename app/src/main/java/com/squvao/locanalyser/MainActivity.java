904a1267c6b6508621b9f5f0c9c76d8ee0623df0
12ase <12ase@kings-school.co.uk>
2016-09-08 17:57:40 +0100

    Design changes in Settings

--- app/src/main/java/com/squvao/locanalyser/MainActivity.java
+++ app/src/main/java/com/squvao/locanalyser/MainActivity.java
@@ -44,14 +44,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initFloatingActionButton() {
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 
     private void initToolbar() {
