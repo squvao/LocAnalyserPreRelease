5026ff9f6ff94568ab94f441bb1e86a09ece985e
12ase <12ase@kings-school.co.uk>
2016-09-12 21:18:13 +0100

    Added a divider into settings, commented on Main Activity code

--- app/src/main/java/com/squvao/locanalyser/MainActivity.java
+++ app/src/main/java/com/squvao/locanalyser/MainActivity.java
@@ -17,96 +17,140 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private Toolbar toolbar;
-    private DrawerLayout drawerLayout;
-    private Button buttonPlay;
+    private Toolbar toolbar; //Поле характеризующее тулбар(панель хранящая заголовок, тогл, меню)
+    private DrawerLayout drawerLayout; //Поле характеризующее ящик
+    private Button buttonPlay; //Поле характеризующее баттон
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        initComponents();
+        setContentView(R.layout.activity_main); //Метод связи activity_main.xml с кодом Java
+        initComponents(); //Метод инициализации компонентов
     }
 
     private void initComponents() {
-        initToolbar();
-        initFloatingActionButton();
-        initNavigationView();
+        initToolbar(); //Метод инициализации Тулбара
+        initFloatingActionButton();//Инициализация круглой кнопки
+        initNavigationView();//Инициализация меню навигации
+        initButtonPlay();//Инициализация кнопки play
+    }
 
+    private void initButtonPlay() {
+        /*
+        Метод findViewById осуществляет поиск эллемента по id
+        Возвращает общий объект View, поэтому необходимо произвести операцию приведения типов
+        Тем самым прировняв к переменной кнопки объект кнопку
+         */
         buttonPlay = (Button) findViewById(R.id.content_main_button_play);
+        /*
+        Метод setOnClickListener устанавливает для данной кнопки
+        обработчик события нажатия на эту кнопку.
+        В этот метод мы передаем анонимный объект анонимного внутреннего класса
+         */
         buttonPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                /*
+                Заводим объект Intent. Он предназначается для того,
+                чтобы запустить новое Activity. В его конструктор
+                мы передаем контекст- MainActivity.this и класс того
+                Activity, которое зотим запустить.
+                 */
                 Intent intent = new Intent(MainActivity.this,MapsActivity.class);
-                startActivity(intent);
+                startActivity(intent); //Метод запуска Activity по объекту intent
             }
         });
     }
 
     private void initFloatingActionButton() {
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });*/
+        });
     }
 
     private void initToolbar() {
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);//Нашли тулбар по id
+        setSupportActionBar(toolbar);//Установили програмно найденный тулбар в качестве основного.
     }
 
     private void initNavigationView() {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        /*
+        Завели объект ActionBarDrawerToggle (три полосы для выдвижения меню навигации)
+        В конструктор передаем контекст- данную activity, drawerLayout, Toolbar, строку открыть,
+        строку закрыть.
+         */
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,
                 R.string.action_bar_drawer_toggle_open,R.string.action_bar_drawer_toggle_close);
+        /*
+        добавление для DrawerLayout обработчика выдвижения
+        ящика (три полоски начинаю вращаться по часовой стрелке при выдвижения ящика)
+        */
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState(); // синхронизация с NavigationView
         NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
         /*
         Установка обработчика нажатия на эллемент NavigationView
-         */
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(MenuItem item) { //обрабатывающий метод (выполняется тогда, когда пользователь выбрал эллемент меню)
-                drawerLayout.closeDrawers(); // закрывает полку
-                int id = item.getItemId();
-                switch (id){
-                    case R.id.sub_menu_navigation_view_about_us:
-                        Intent intent = new Intent(MainActivity.this,AboutUsActivity.class);
-                        startActivity(intent);
-                        break;
-                    case R.id.menu_navigation_view_help:
-                        Intent intent2 = new Intent(MainActivity.this,HelpActivity.class);
-                        startActivity(intent2);
-                        break;
-                    case R.id.menu_navigation_view_feedback:
-                        ShareCompat.IntentBuilder.from(MainActivity.this).setType("message/rfc822")
-                                .addEmailTo(getString(R.string.main_activity_navigation_item_feedback_email))
-                                .setSubject(getString(R.string.app_name))
-                                .setText(getString(R.string.main_activity_navigation_item_feedback_message))
-                                .setChooserTitle(getString(R.string.main_activity_navigation_item_feedback_title))
-                                .startChooser();
-                        break;
-                    case R.id.menu_navigation_view_settings:
-                        Intent intent1 = new Intent(MainActivity.this,SettingsActivity.class);
-                        startActivity(intent1);
-                        break;
-                }
-                return true;
-            }
-        });
-    }
+        В методе OnNavigationItemSelected в зависимости от того,
+        какой пункт меню был выбран (определяется по идентификатору пункта меню)
+        выполняется то или иное действие.
 
+        Установка обработчика осуществояется в том случае, если метод FindViewById
+        смог найти NavigationView по id и соответсыенно NavigationView не равно null
+         */
+        if (navigationView != null) {
+            navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(MenuItem item) { //обрабатывающий метод (выполняется тогда, когда пользователь выбрал эллемент меню)
+                    drawerLayout.closeDrawers(); // закрывает полку
+                    int id = item.getItemId();
+                    switch (id){
+                        case R.id.sub_menu_navigation_view_about_us:
+                            Intent intent = new Intent(MainActivity.this,AboutUsActivity.class);
+                            startActivity(intent);
+                            break;
+                        case R.id.menu_navigation_view_help:
+                            Intent intent2 = new Intent(MainActivity.this,HelpActivity.class);
+                            startActivity(intent2);
+                            break;
+                        case R.id.menu_navigation_view_feedback:
+                            /*
+                            Запуск activity для отправки mail
+                             */
+                            ShareCompat.IntentBuilder.from(MainActivity.this).setType("message/rfc822")
+                                    .addEmailTo(getString(R.string.main_activity_navigation_item_feedback_email))
+                                    .setSubject(getString(R.string.app_name))
+                                    .setText(getString(R.string.main_activity_navigation_item_feedback_message))
+                                    .setChooserTitle(getString(R.string.main_activity_navigation_item_feedback_title))
+                                    .startChooser();
+                            break;
+                        case R.id.menu_navigation_view_settings:
+                            Intent intent1 = new Intent(MainActivity.this,SettingsActivity.class);
+                            startActivity(intent1);
+                            break;
+                    }
+                    return true;
+                }
+            });
+        }
+    }
+    /*
+    Привязка menu_main.xml к коду
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
+    /*
+    Обработка пунктов меню в зависимости от id
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
